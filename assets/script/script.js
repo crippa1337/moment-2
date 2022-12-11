3e8eecadd19b44d34adee127155ee7ebca79ab90
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2022-12-11 21:38:23 +0100

    fixed input

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,54 +5,50 @@ const sliderText = document.getElementById('dom-area__slider-text');
 
 let html = input.value;
 let css = '<style>@keyframes rotate { from {transform: rotate(0deg);} to {transform: rotate(360deg);}} #dom-area__output {animation: rotate 5s linear alternate-reverse infinite; display: flex; justify-content: center; align-items: center; background-color: #000; color: #fff; width: 30%; height: 10vh; overflow: hidden;}</style>'
-let jsEnabled = false;
 
 input.addEventListener('input', updateDOM);
 slider.addEventListener('input', updateSlider);
 
-function updateDOM() {
-    html = input.value;
-    updateSlider();
-};
 
-
-function updateVal() {
+function updateVal(type) {
     text = sliderText.innerText;
-    text = text.slice(0, -1);
-    text += slider.value;
+    text = text.replace(/1 HTML|2 CSS|3 SCRIPT|0 NONE/g, '')
+    text += ' ' + slider.value + type;
     sliderText.innerText = text;
 };
 
 function updateSlider() {
-    updateVal()
-    input.setAttribute("disabled", true);
-
     switch (parseInt(slider.value)) {
         case 0: {
-            jsEnabled = false;
             content.innerHTML = '';
-            console.log(slider.value, jsEnabled)
+            input.setAttribute("disabled", true);
+            updateVal(" NONE");
             break;
         };
         case 1: {
-            jsEnabled = false;
             content.innerHTML = html;
             content.innerText = html + css;
-            console.log(slider.value, jsEnabled)
+            input.setAttribute("disabled", true);
+            updateVal(" HTML");
             break;
         };
         case 2: {
-            jsEnabled = false;
             content.innerHTML = html + css;
-            console.log(slider.value, jsEnabled)
+            input.setAttribute("disabled", true);
+            updateVal(" CSS");
             break;
         };
         case 3: {
             jsEnabled = true;
             content.innerHTML = html + css;
             input.removeAttribute("disabled");
-            console.log(slider.value, jsEnabled)
+            updateVal(" SCRIPT");
             break;
         };
     };
+};
+
+function updateDOM() {
+    html = input.value;
+    updateSlider();
 };
